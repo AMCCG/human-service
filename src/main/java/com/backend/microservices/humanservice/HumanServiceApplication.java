136d026c07060d03038ac73023bdab1d964f9c14
apisit.a <amornchanchaigul@gmail.com>
2024-01-14 15:21:19 +0700

    initial project

---
+++ src/main/java/com/backend/microservices/humanservice/HumanServiceApplication.java
@@ -0,0 +1,13 @@
+package com.backend.microservices.humanservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/microservices/humanservice/HumanServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.microservices.humanservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
