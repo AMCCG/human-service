c807d39f29f41942028999c93bf20d54ca8d3f2a
apisit.a <amornchanchaigul@gmail.com>
2024-01-14 22:09:13 +0700

    create first controller

---
+++ src/main/java/com/backend/microservices/humanservice/model/HumanModel.java
@@ -0,0 +1,23 @@
+package com.backend.microservices.humanservice.model;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class HumanModel {
+    private int id;
+    private String idCard;
+    private String thaiTitle;
+    private String thaiFirstName;
+    private String thaiLastName;
+    private String englishTitle;
+    private String englishFirstName;
+    private String englishLastName;
+    private Date dateOfBirth;
+    private String address;
+    private String email;
+    private String phone;
+    private String profileImageId;
+
+}
